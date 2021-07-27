94785501db33abf59deafb1f82dfb97450d0e31f
Riddhima Singhal <59498358+riddhima270406@users.noreply.github.com>
2021-07-27 18:46:34 +0530

    initial commit

---
+++ C142 project/storage.py
@@ -0,0 +1,12 @@
+import csv
+
+all_movies = []
+
+with open('articles.csv') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_movies = data[1:]
+
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
